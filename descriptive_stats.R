b87870c5770689ae28b80f1ab8cdaed318260b5e
Minasian <minasyanvaagn@mail.ru>
2018-05-07 01:36:43 +0300

    -

--- descriptive_stats.R
+++ descriptive_stats.R
@@ -1,11 +1,8 @@
+Sys.setenv(PATH="%PATH%;C:/Rtools/gcc-4.6.3/bin;c:/Rtools/bin")
 library(forecast)
 library(quantmod)
 library(dplyr)
-remove.packages('Rcpp')
-# install.packages('dplyr')
-# install.packages('rugarch')
-# install.packages('timeSeries')
-# install.packages('tseries')
+library(psych )
 library(timeSeries)
 library(tseries)
 library(xts)
@@ -13,8 +10,16 @@ library(rugarch)
 df=read.csv('all-crypto-currencies/crypto-markets.csv')
 unique(df[,'name'])
 df_new=df[c('name','date','open','high','low','close','volume','market')]
-df_new=df_new[df_new[,'name']=='Bitcoin' | df_new[,'name']=='Ethereum',]
-logturnover=log(df_new['volume']+0.00000255)
+grouped=group_by(df_new, name) %>%
+  summarise(sum=sum(market), sd=sd(market))
+grouped=grouped[ order(-grouped[,2]), ]
+percentages=grouped['sum']/sum(grouped[,2])
+sum(percentages[1:3,]) #### FIRST 3 currencies account for 72% of total market cap 
+named_of_cryptos=lapply(c(grouped[1:5,1]), as.character)
+# df_to_work_with=df_new[df_new[,'name'] %in% named_of_cryptos$name,] 
+df_to_work_with_2=df_new[df_new[,'name'] %in% c("Bitcoin",
+                                              "Ethereum",
+                                              "Ripple"),]
 
-group_by(df_new, name) %>%
-  summarise(mean=mean(age), sd=sd(age))
+write.csv(df_to_work_with_2, file = "df_to_prepare_for_multiple_analysis.csv")
+     
---
+++ prepare_data.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+# 1. Load DS and make pivot table
+df=pd.read_csv('df_to_prepare_for_multiple_analysis.csv')
+df.head()
+df=df[['date','name','volume','close']]
+df_last=pd.pivot_table(df,values=['volume','close'],index=['date'],columns=['name'])
+df_last.index=pd.to_datetime(df_last.index)
+sorted(list(np.unique(df_last[np.any(df_last.isnull(),axis=1)].index)))
+df_last=df_last.dropna()
+list(df_last)
+
+# 2. Prepare Rt: returns (shor will be Rt) for each cryprocyrrency
+Rt=df_last.iloc[:,:3]
+Rt=Rt.pct_change()
+Rt[pd.isnull(Rt)]=0
+Rt.columns=['R_'+i[1] for i in list(Rt)]
+
+# 3. Prepare vt : cryptocyrrency 50-day de-trended measure of trading activity as described in
+# "Dynamic volume-Return relation of individual stocks. Rev. Financial Stud. 15, 1005–1047."
+Vt_temp=df_last.iloc[:,3:]
+Vt_temp=np.log(Vt_temp+0.00000255)
+trend_lasts=50
+Vt=[]
+for i in range(0,Vt_temp.shape[0]):
+    i=201
+    if i<trend_lasts:
+        Vt.append(np.array([0.,0.,0.]))
+        continue
+    Vt.append(Vt_temp.iloc[i,:].as_matrix()-np.mean(Vt_temp.iloc[i-trend_lasts:i-1,:]).as_matrix())
+Vt=pd.DataFrame(np.asarray(Vt)).set_index(Rt.index)
+Vt.columns=list(Vt_temp)
+Vt.columns=['V_'+i[1] for i in list(Vt)]
+
+# 4. Prepare Rt x vt
+RVt=pd.DataFrame(np.asarray(Vt)).set_index(Rt.index)
+RVt.columns=['RV_'+i for i in ['Bitcoin','Ethereum','Ripple']]
+
+# 5. Save tables
+res_table=pd.concat([RVt,Vt,Rt],axis=1)
+res_table.to_csv('df_prepared_for_multiple_analysis.csv',header=True,index=True)
